9da0ef668a40e822150f9516c8451feab6df793f
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-08-14 10:34:36 -0300

    Passando variaveis do ts para o html. Usando o for do angular

--- AngularJSBasic/src/app/app.component.ts
+++ AngularJSBasic/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngularJSBasic';
+  public todos: any[] = [];
+  public title: String = 'Minhas Tarefas';
+  constructor() {
+    this.todos.push('passar com o carro');
+    this.todos.push('passar com o aviao');
+    this.todos.push('passar com o onibus');
+    this.todos.push(3456);
+    this.todos.push(new Date());
+    this.todos.push({test:"wew"});
+
+
+  }
 }
